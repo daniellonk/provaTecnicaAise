bd9901cfd5efb5ab3c3157763782674b34e3320a
Daniel Lonkoski <daniel.lonkoski@elotech.com.br>
2024-02-20 10:15:02 -0300

    Finalizado teste da pessoa

--- src/main/java/com/br/daniel/provaTecnicaAise/repository/DebitoParcelaPagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.br.daniel.provaTecnicaAise.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DebitoParcelaPagamentoRepository extends JpaRepository<DebitoParcelaPagamento, Long> {
-
-}
